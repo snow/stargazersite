12521e188e69605ba02eb2374aa94f0e7625e69d
Snow Hellsing <snow.hellsing@gmail.com>
2012-02-16 16:47:01 +0800

    working in progress

--- stargazer/demo/data.py
+++ stargazer/demo/data.py
@@ -47,7 +47,7 @@ j_d_yunnan = {
             'cover_uri': '/s/stargazer/demo/dragoncd/yunnan/6492382943_ea2a32562a_b.jpg',
             'place': '大理',
             'date': '2011-10-15',
-            'abspath_full': '/story/v/k/long_lake/',
+            'abspath_full': '/stories/v/k/long_lake/',
         },
         {
             'title': '日出后才起床的灰雁们',
@@ -56,7 +56,7 @@ j_d_yunnan = {
             'cover_uri': '/s/stargazer/demo/dragoncd/yunnan/6851869567_17f29f704d_b.jpg',
             'place': '大理',
             'date': '2011-10-15',
-            'abspath_full': '/story/v/j/long_lake/',
+            'abspath_full': '/stories/v/j/long_lake/',
         },
         {
             'title': '大理第一天感受',
@@ -65,7 +65,7 @@ j_d_yunnan = {
             'cover_uri': '',
             'place': '大理',
             'date': '2011-10-15',
-            'abspath_full': '/story/v/i/long_lake/',
+            'abspath_full': '/stories/v/i/long_lake/',
         },
         {
             'title': '洱海的第一班游船',
@@ -74,7 +74,7 @@ j_d_yunnan = {
             'cover_uri': '/s/stargazer/demo/dragoncd/yunnan/6851868381_f8fe2c7452_b.jpg',
             'place': '洱海',
             'date': '2011-10-16',
-            'abspath_full': '/story/v/h/long_lake/',
+            'abspath_full': '/stories/v/h/long_lake/',
         },
         {
             'title': '古镇小院的老人',
@@ -83,7 +83,7 @@ j_d_yunnan = {
             'cover_uri': '/s/stargazer/demo/dragoncd/yunnan/6579084387_3f9eabcbeb_b.jpg',
             'place': '喜州古镇',
             'date': '2011-10-16',
-            'abspath_full': '/story/v/g/long_lake/',
+            'abspath_full': '/stories/v/g/long_lake/',
         },
         {
             'title': '喜州古镇民居,大理',
@@ -92,7 +92,7 @@ j_d_yunnan = {
             'cover_uri': '/s/stargazer/demo/dragoncd/yunnan/6579083731_45552dfbd5_b.jpg',
             'place': '喜州古镇',
             'date': '2011-10-16',
-            'abspath_full': '/story/v/f/long_lake/',
+            'abspath_full': '/stories/v/f/long_lake/',
         },
         {
             'title': '苍山索道旁的巨型棋盘',
@@ -101,7 +101,7 @@ j_d_yunnan = {
             'cover_uri': '/s/stargazer/demo/dragoncd/yunnan/6519832019_78ccc44608_b.jpg',
             'place': '苍山',
             'date': '2011-10-17',
-            'abspath_full': '/story/v/e/long_lake/',
+            'abspath_full': '/stories/v/e/long_lake/',
         },
         {
             'title': 'Three towers of the Chongsheng Temple, Dali',
@@ -110,7 +110,7 @@ j_d_yunnan = {
             'cover_uri': '/s/stargazer/demo/dragoncd/yunnan/6519831633_0f2551a900_b.jpg',
             'place': '大理',
             'date': '2011-10-18',
-            'abspath_full': '/story/v/d/long_lake/',
+            'abspath_full': '/stories/v/d/long_lake/',
         },
         {
             'title': 'trees near Longkan Wharf, Dali',
@@ -119,7 +119,7 @@ j_d_yunnan = {
             'cover_uri': '/s/stargazer/demo/dragoncd/yunnan/6519831409_bde48d6d1f_b.jpg',
             'place': '大理',
             'date': '2011-10-18',
-            'abspath_full': '/story/v/c/long_lake/',
+            'abspath_full': '/stories/v/c/long_lake/',
         },
         {
             'title': 'Waves, Erhai Lake, Dali',
@@ -128,7 +128,7 @@ j_d_yunnan = {
             'cover_uri': '/s/stargazer/demo/dragoncd/yunnan/6519830951_84252857e4_b.jpg',
             'place': '大理',
             'date': '2011-10-18',
-            'abspath_full': '/story/v/b/long_lake/',
+            'abspath_full': '/stories/v/b/long_lake/',
         },
         {
             'title': '才村码头的鸬鹚/ Cormorant, Caicun Wharf, Dali',
@@ -137,7 +137,7 @@ j_d_yunnan = {
             'cover_uri': '/s/stargazer/demo/dragoncd/yunnan/6508752347_dd5982a357_b.jpg',
             'place': '大理',
             'date': '2011-10-18',
-            'abspath_full': '/story/v/a/long_lake/',
+            'abspath_full': '/stories/v/a/long_lake/',
         },
         {
             'title': '龙龛码头的鸟儿/ Bird walk on the Longkan Wharf,Dali',
@@ -146,7 +146,7 @@ j_d_yunnan = {
             'cover_uri': '/s/stargazer/demo/dragoncd/yunnan/6508750923_b8c316e959_b.jpg',
             'place': '大理',
             'date': '2011-10-18',
-            'abspath_full': '/story/v/9/long_lake/',
+            'abspath_full': '/stories/v/9/long_lake/',
         },
         {
             'title': 'flower on the Haishe Island',
@@ -155,7 +155,7 @@ j_d_yunnan = {
             'cover_uri': '/s/stargazer/demo/dragoncd/yunnan/6492389853_a9bf06b9c0_b.jpg',
             'place': '海蛇岛',
             'date': '2011-10-19',
-            'abspath_full': '/story/v/8/long_lake/',
+            'abspath_full': '/stories/v/8/long_lake/',
         },
         {
             'title': 'Erhai Lake',
@@ -164,7 +164,7 @@ j_d_yunnan = {
             'cover_uri': '/s/stargazer/demo/dragoncd/yunnan/6492388527_673980781f_b.jpg',
             'place': '洱海',
             'date': '2011-10-20',
-            'abspath_full': '/story/v/7/long_lake/',
+            'abspath_full': '/stories/v/7/long_lake/',
         },
         {
             'title': 'Sunrise of Erhai Lake/洱海日出',
@@ -175,7 +175,7 @@ j_d_yunnan = {
             'cover_uri': '/s/stargazer/demo/dragoncd/yunnan/6492386783_3b08ca6351_b.jpg',
             'place': '洱海',
             'date': '2011-10-20',
-            'abspath_full': '/story/v/6/long_lake/',
+            'abspath_full': '/stories/v/6/long_lake/',
         },
         {
             'title': '苍山七龙池瀑布群，大理',
@@ -184,11 +184,12 @@ j_d_yunnan = {
             'cover_uri': '/s/stargazer/demo/dragoncd/yunnan/6492385047_f2eece2161_b.jpg',
             'place': '大理',
             'date': '2011-10-21',
-            'abspath_full': '/story/v/5/long_lake/',
+            'abspath_full': '/stories/v/5/long_lake/',
         },
     ]
 }
 j_d_yunnan['track'] = _build_track_for_journey(j_d_yunnan)
+j_d_yunnan['cover_uri'] = j_d_yunnan['stories'][0]['cover_uri']
 
 j_d_jiuzhai = {
     'title': '九寨沟',
@@ -201,7 +202,7 @@ j_d_jiuzhai = {
             'cover_uri': '/s/stargazer/demo/dragoncd/jiuzhai/6191963417_3510fb7486_b.jpg',
             'place': '珍珠滩瀑布',
             'date': '2011-9-7',
-            'abspath_full': '/story/v/4/long_lake/',
+            'abspath_full': '/stories/v/4/long_lake/',
         },
         {
             'title': '树正瀑布',
@@ -211,7 +212,7 @@ j_d_jiuzhai = {
             'cover_uri': '/s/stargazer/demo/dragoncd/jiuzhai/6192455146_89107eb535_b.jpg',
             'place': '树正瀑布',
             'date': '2011-9-7',
-            'abspath_full': '/story/v/3/long_lake/',
+            'abspath_full': '/stories/v/3/long_lake/',
         },
         {
             'title': '五花海/ Wuhua Lake , Jiuzhaigou Valley',
@@ -220,7 +221,7 @@ j_d_jiuzhai = {
             'cover_uri': '/s/stargazer/demo/dragoncd/jiuzhai/6192453290_799b270318_b.jpg',
             'place': '五花海',
             'date': '2011-9-8',
-            'abspath_full': '/story/v/2/long_lake/',
+            'abspath_full': '/stories/v/2/long_lake/',
         },
         {
             'title': '长海/ Long Lake , Jiuzhaigou Valley',
@@ -229,7 +230,7 @@ j_d_jiuzhai = {
             'cover_uri': '/s/stargazer/demo/dragoncd/jiuzhai/6191933951_6c517c8445_b.jpg',
             'place': '长海',
             'date': '2011-9-8',
-            'abspath_full': '/story/v/1/long_lake/',
+            'abspath_full': '/stories/v/1/long_lake/',
         },
     ]
 }
@@ -253,6 +254,25 @@ people_drangoncd['journey_count'] = len(people_drangoncd['journeys'])
 people_drangoncd['story_count'] = sum([len(j['stories']) for j in people_drangoncd['journeys']])
 people_drangoncd['place_count'] = _count_places_for_people(people_drangoncd)
 
+people_drangoncd['cover_uri'] = people_drangoncd['avatar_uri']
+people_drangoncd['title'] = people_drangoncd['fullname']
+
+# ---------
+people_maklu = {
+    'username': 'maklu',
+    'fullname': '卢卫彬',
+    'avatar_uri': '/s/stargazer/demo/maklu/5949576750_597801a57d_b.jpg',
+    'abspath_full': '/people/v/maklu/',
+    'journeys': [],              
+}
+
+people_maklu['journey_count'] = len(people_maklu['journeys'])
+people_maklu['story_count'] = sum([len(j['stories']) for j in people_maklu['journeys']])
+people_maklu['place_count'] = _count_places_for_people(people_maklu)
+
+people_maklu['cover_uri'] = people_maklu['avatar_uri']
+people_maklu['title'] = people_maklu['fullname']
+
 #
 # place
 # ------
@@ -264,6 +284,20 @@ place_dali = {
 }
 place_dali['story_count'] = sum([len(group[1]) for group in place_dali['stories_by_date']])
 
+place_dali['cover_uri'] = place_dali['stories_by_date'][0][1][0]['cover_uri']
+place_dali['title'] = place_dali['name']
+
+#-------
+place_erhai = {
+    'name': '洱海',
+    'abspath_full': '/places/v/2/洱海/',
+    'stories_by_date': _extract_stories_by_place('洱海', journey_list)
+}
+place_erhai['story_count'] = sum([len(group[1]) for group in place_erhai['stories_by_date']])
+
+place_erhai['cover_uri'] = place_erhai['stories_by_date'][0][1][0]['cover_uri']
+place_erhai['title'] = place_erhai['name']
+
 #
 # context
 # ------
@@ -273,4 +307,13 @@ context_data = {
     'journey_list': journey_list,
     'journey': j_d_yunnan,
     'place': place_dali,
+    'story': j_d_yunnan['stories'][0]
 }
+
+#all_bricks = j_d_yunnan['stories']
+#all_bricks.extend(j_d_jiuzhai['stories'])
+all_bricks = [people_drangoncd, place_dali, j_d_yunnan]
+all_bricks.extend(j_d_jiuzhai['stories'])
+all_bricks.extend([people_maklu, place_erhai])
+all_bricks.extend(j_d_yunnan['stories'])
+
--- stargazer/views.py
+++ stargazer/views.py
@@ -1,7 +1,7 @@
 import django.views.generic as gv
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 
-from stargazer.demo.data import context_data as demo_context
+from stargazer.demo.data import all_bricks
 
 class RootV(gv.TemplateView):
     ''''''
@@ -12,13 +12,15 @@ class IndexV(gv.TemplateView):
     ''''''
     template_name = 'stargazer/pg/index.html'
     
-    def get_context_data(self, username):
-        return demo_context    
+    def get_context_data(self):
+        return {
+            'brick_list': all_bricks
+        }    
     
     
 class DashboardV(gv.TemplateView):
     ''''''
     template_name = 'stargazer/pg/dashboard.html'       
     
-    def get_context_data(self, username):
+    def get_context_data(self):
         return demo_context     
