532b3e05d16671d49baf975bb730aba2534bd1f6
Snow Hellsing <snow.hellsing@gmail.com>
2012-02-16 19:10:21 +0800

    mf

--- settings_common.py
+++ settings_common.py
@@ -49,7 +49,7 @@ USE_L10N = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = '{}/webroot/media/'.format(PROJECT_ROOT)
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -60,7 +60,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = PROJECT_ROOT + '/webroot/s/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
